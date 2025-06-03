cba4913329c4236b8599086cec410fa6690d35a0
Rizqi Akbar <rizqiakbarsukirmanp@gmail.com>
2025-06-03 18:33:14 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,39 @@
-#include "shell.h"
 #include "kernel.h"
 
-int main() {
-  clearScreen();
-  shell();
+void printString(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        interrupt(0x10, 0x0E00 + str[i], 0, 0, 0);
+        i++;
+    }
 }
 
-void printString(char *str)
-{
-  //TODO: Implementasi fungsi untuk mencetak string
+void readString(char *buf) {
+    char c;
+    int i = 0;
+
+    while (1) {
+        c = interrupt(0x16, 0, 0, 0, 0);
+        if (c == 13) { // Enter
+            buf[i] = '\0';
+            printString("\r\n");
+            break;
+        } else if (c == 8) { // Backspace
+            if (i > 0) {
+                i--;
+                printString("\b \b");
+            }
+        } else {
+            buf[i++] = c;
+            interrupt(0x10, 0x0E00 + c, 0, 0, 0);
+        }
+    }
 }
 
-void readString(char *buf)
-{
-  //TODO: Implementasi fungsi untuk membaca string
-}
-
-void clearScreen()
-{
-  //TODO: Implementasi fungsi untuk membersihkan layar
+void clearScreen() {
+    int i;
+    for (i = 0; i < 25 * 80; i++) {
+        putInMemory(0xB800, i * 2, ' ');
+        putInMemory(0xB800, i * 2 + 1, 0x07);
+    }
 }
